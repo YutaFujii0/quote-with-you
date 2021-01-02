f39fce50d97f2ab52aaddd30074bd6728e7041fc
YutaFujii0 <y.f.mar2@gmail.com>
2021-01-03 03:37:46 +0900

    Configure meta tags in en

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,56 @@ const nuxtConfig: NuxtConfig = {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'quotes-picker',
+    title: 'Your 2021 Quote',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Randomly pick a quote for you that might encourage you in 2021.',
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@__yutafujii__' },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: `"Your 2021's Quote"`,
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'Randomly pick a quote for you that might encourage you in 2021.',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://20201219-cloudfront-demo.yutafujii.net/ogp.png',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://20201219-cloudfront-demo.yutafujii.net',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: `"Your 2021's Quote"` },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Randomly pick a quote for you that might encourage you in 2021.',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://20201219-cloudfront-demo.yutafujii.net/ogp.png',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
